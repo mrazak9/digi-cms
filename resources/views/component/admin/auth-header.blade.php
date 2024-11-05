b3220749eac816e699a9c017d56800cacfa2d7f9
mrazak9 <muhamadrozak9@gmail.com>
2024-11-05 16:28:29 +0700

    update logo resource

--- resources/views/component/admin/auth-header.blade.php
+++ resources/views/component/admin/auth-header.blade.php
@@ -1,3 +1,3 @@
  <div class="login-brand">
-     <img src="{{ asset('storage/' . $gen_setting['logo'] ?? '') }}" alt="logo" width="200">
+     <img src="{{ asset('storage/' . $gen_setting['profil_company_logo'] ?? '') }}" alt="logo-digi-tekno" width="200">
  </div>
